e18b38274daa72c00e32ebe450ba47b549395104
franckDev <developpeur16700@gmail.com>
2016-03-23 16:30:52 +0100

    branche

--- Application/Symfony/README.md
+++
@@ -1,4 +0,0 @@
-Symfony
-=======
-
-A Symfony project created on February 3, 2016, 12:48 am.
--- Tests/QrCodeGenerate/qrcode.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	include "phpqrcode/qrlib.php";
-	$bgcolor = 0x33CCCC;
-	$fgcolor = 0x3333CC;
-	// Qrcode::png("http://www.crazyws.fr/"); // QrCode affiché à l'écran
-	// Qrcode::png("http://www.crazyws.fr/", "crazyws.png", "L", 4, 4); // QrCode enregistrer sous le nom de crazyws.png à la racine
-	Qrcode::svg("http://www.crazyws.fr/", "crazyws.svg", "L", 4, 4, false, $bgcolor, $fgcolor); // QrCode enregistrer sous le nom de crazyws.svg à la racine
-?>
